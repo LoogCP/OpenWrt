78584d24a25d6d14e8b918aec4b8da447fb30efb
LoogCP <lp18883370076@gmail.com>
2025-03-24 20:55:41 +0800

    Update diy-iw24.sh

--- diy-iw24.sh
+++ diy-iw24.sh
@@ -16,6 +16,9 @@ git clone -b dev/main #https://github.com/muink/homeproxy.git package/homrproxy 
 git clone -b dev https://github.com/immortalwrt/homeproxy.git package/homrproxy  #使用immortalwrt/dev分支
 
 # 添加额外插件
+##nikki
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 #添加dae支持
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
